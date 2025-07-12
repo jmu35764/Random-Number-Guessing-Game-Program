1a9e92c7ec76c4ca80102d5ac9ab474411bdb3ea
jm underwood <jm.underwood@hotmail.com>
2025-07-11 20:17:36 -0400

    Add project files.

---
+++ Random Number Guessing Game Program.cpp
@@ -0,0 +1,46 @@
+// Random Number Guessing Game Program.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <random>
+using namespace std;
+
+int main()
+{
+    
+    // Random number engine
+    random_device engine;
+    uniform_int_distribution<> dist(1, 1000);
+
+    int rand_num = dist(engine);
+
+    int input;
+    int count = 0;
+
+    cout<< "A random number has been generated. Can you guess it? \n";
+    cin >> input;
+
+
+    while (input != rand_num)
+    {
+        count += 1;
+        
+        if (input > rand_num)
+        {
+            cout << "Too high, try again. \n";
+        }
+
+        if (input < rand_num)
+        {
+            cout << "Too low, try again. \n";
+        }
+
+    }
+
+    cout << "Congradulations, you got the number correct! \n";
+    cout << "It took " << count << " attempts.";
+
+    
+}
+
+
